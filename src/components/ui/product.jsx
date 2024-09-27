40347dc419bb05bbcff93c74bdda020757870298
AynMedia <info@ayntech.in>
2024-09-27 12:39:33 +0530

    slug

--- src/components/ui/product.jsx
+++ src/components/ui/product.jsx
@@ -1,16 +1,17 @@
 /** @format */
-'use client';
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 import {
   Card,
   CardHeader,
   CardTitle,
   CardContent,
   CardFooter,
-} from '@/components/ui/card';
-import Image from 'next/image';
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
+} from "@/components/ui/card";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Product = ({ products }) => {
   const [visibleProducts, setVisibleProducts] = useState(6);
@@ -20,37 +21,44 @@ const Product = ({ products }) => {
   };
   return (
     <>
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6 max-w-6xl mx-auto my-12'>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6 max-w-6xl mx-auto my-12">
         {products.slice(0, visibleProducts).map((product, index) => (
-          <Card className='shadow-md' key={index}>
-            <CardHeader>
-              {/* <Image
+          <Link
+            key={index}
+            href={`/products/${product.title
+              .toLowerCase()
+              .replace(/\s+/g, "-")}`}
+          >
+            <Card className="shadow-md" key={index}>
+              <CardHeader>
+                {/* <Image
           src={product.image}
           alt={product.title}
           width={400}
           height={300}
         /> */}
-              <Image
-                src='https://assets.aceternity.com/macbook.png'
-                alt={product.title}
-                width={400}
-                height={300}
-              />
-            </CardHeader>
-            <CardContent>
-              <CardTitle>{product.title}</CardTitle>
-              {/* <p>{product.description}</p> */}
-            </CardContent>
-            <CardFooter>
-              <Badge variant='secondary'>{product.location}</Badge>
-              <Badge variant='secondary'>{product.category}</Badge>
-            </CardFooter>
-          </Card>
+                <Image
+                  src="https://assets.aceternity.com/macbook.png"
+                  alt={product.title}
+                  width={400}
+                  height={300}
+                />
+              </CardHeader>
+              <CardContent>
+                <CardTitle>{product.title}</CardTitle>
+                <p>{product.description}</p>
+              </CardContent>
+              <CardFooter>
+                <Badge variant="secondary">{product.location}</Badge>
+                <Badge variant="secondary">{product.category}</Badge>
+              </CardFooter>
+            </Card>
+          </Link>
         ))}
       </div>
-      <div className='flex my-24 justify-center max-w-6xl mx-auto'>
+      <div className="flex my-24 justify-center max-w-6xl mx-auto">
         {visibleProducts < products.length && (
-          <Button variant='outline' size='lg' onClick={loadMore}>
+          <Button variant="outline" size="lg" onClick={loadMore}>
             Load More
           </Button>
         )}
