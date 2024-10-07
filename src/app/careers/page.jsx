cba1ec6198070a8d498b5869c3cc59654146d641
AynMedia <info@ayntech.in>
2024-10-07 15:21:36 +0530

    metadata

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -1,18 +1,43 @@
 /** @format */
 
-import Team from '@/components/team';
-import PageHeader from '@/components/ui/page_header';
-import React from 'react';
+import Team from "@/components/team";
+import PageHeader from "@/components/ui/page_header";
+import React from "react";
+
+const metadata = {
+  title: "Careers - SD Pneumatics",
+  description:
+    "SD Pneumatics is a leading provider of high-quality pneumatic solutions, specializing in air compressors, air dryers, and advanced industrial automation systems. Our commitment to innovation, reliability, and customer satisfaction ensures efficient and sustainable solutions for diverse industrial needs.",
+  keywords:
+    "SD Pneumatics, Pneumatics, Compressors, Air Dryers, Industrial Automation, careers",
+  opengraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://sdpnematics.com",
+    title: "Careers - SD Pneumatics",
+    description:
+      "SD Pneumatics is a leading provider of high-quality pneumatic solutions, specializing in air compressors, air dryers, and advanced industrial automation systems. Our commitment to innovation, reliability, and customer satisfaction ensures efficient and sustainable solutions for diverse industrial needs.",
+    images: [
+      {
+        url: "https://sdpnematics.com/images/SD_Pneumatics.webp",
+        width: 800,
+        height: 600,
+        alt: "SD Pneumatics",
+      },
+    ],
+    site_name: "SD Pneumatics",
+  },
+};
 
 const page = () => {
   return (
     <>
-      <PageHeader title='Careers' imageSrc='/images/Careers_Img.webp' />
-      <div className='max-w-6xl my-24 mx-4 md:mx-auto'>
-        <h1 className='text-4xl font-bold text-left font-spaceGrotesk mb-2'>
+      <PageHeader title="Careers" imageSrc="/images/Careers_Img.webp" />
+      <div className="max-w-6xl my-24 mx-4 md:mx-auto">
+        <h1 className="text-4xl font-bold text-left font-spaceGrotesk mb-2">
           Join Our Team
         </h1>
-        <p className='text-lg text-left'>
+        <p className="text-lg text-left">
           Join Our Team at SD Pneumatics, we’re not just shaping the future of
           pneumatics; we’re creating an environment where talent thrives. We
           believe in pushing the boundaries of innovation while maintaining an
@@ -20,7 +45,7 @@ const page = () => {
           passionate, driven, and ready to make an impact, we invite you to
           explore career opportunities with us.
         </p>
-        <div className='text-center my-12 text-2xl font-medium italic'>
+        <div className="text-center my-12 text-2xl font-medium italic">
           No Opennings at the moment, but we will be opening positions in the
           near future.
         </div>
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,28 +1,23 @@
 /** @format */
 
-import PageHeader from '@/components/ui/page_header';
-import React from 'react';
-import Product from '@/components/ui/product';
-import products from '@/lib/products.json';
-import Reavel from '@/lib/reveal';
-
-export const metadata = {
-  title: 'Our Wide Range of Products',
-  description: 'Our Wide Range of Products',
-};
+import PageHeader from "@/components/ui/page_header";
+import React from "react";
+import Product from "@/components/ui/product";
+import products from "@/lib/products.json";
+import Reavel from "@/lib/reveal";
 
 const Page = () => {
   return (
     <div>
       <PageHeader
-        title='Our Products Range'
-        currentPage='Products'
-        imageSrc='/images/productpage.jpg'
+        title="Our Products Range"
+        currentPage="Products"
+        imageSrc="/images/productpage.jpg"
       />
-      <section className='w-full text-center gap-6 my-12'>
-        {' '}
+      <section className="w-full text-center gap-6 my-12">
+        {" "}
         <Reavel>
-          <p className='text-lg text-left max-w-6xl mx-auto'>
+          <p className="text-lg text-left max-w-6xl mx-auto">
             We provide the ideal products for multiple applications and complete
             systems. Therefore, a broad range of pneumatic variations is offered
             for each system component. This complete array of products results
